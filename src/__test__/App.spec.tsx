c1a1402173a567196d81a3f5e90208b0bb8d33ae
山本恵大 <keita@YAM-YAM-MacBook-Air.local>
2026-04-07 16:03:11 +1200

    test-add

---
+++ src/__test__/App.spec.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import App from '../App'
+
+
+test("タイトルあるか",()=>{
+    render(<App />)
+    expect(screen.getByTestId("title")).toHaveTextContent("Cleaning")
+})
