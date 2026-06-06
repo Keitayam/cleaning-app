f9dcd3a59453f134e92f45dae35f4f6efbc44238
Keita Yamamoto <keita77yam@gmail.com>
2026-06-06 15:29:44 +0900

    localstrageのユーザー保存

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -12,15 +12,28 @@ type AuthContextType ={
     logout: ()=>void;
 }
 
+
 const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
+const STORAGE_KEY = "auth_user";
+
+const saveUser = (user:User)=>{
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(user));
+}
+const loadUser =(): User | null =>{
+    const stored = localStorage.getItem(STORAGE_KEY);
+    return stored ? JSON.parse(stored) : null;
+}
+
 export const AuthProvider = ({children}:{children:ReactNode})=>{
-    const [user, setUser]=useState<User | null>(null);
+    const [user, setUser]=useState<User | null>(loadUser);
     const login =(user:User)=>{
         setUser(user);
+        saveUser(user)
     }
     const logout =()=>{
         setUser(null);
+        localStorage.removeItem(STORAGE_KEY);
     }
 
     return(
