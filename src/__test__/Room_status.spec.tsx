1ea3d28cb7a208339cb8c83b791a36f8600b69d2
Keita Yamamoto <keita77yam@gmail.com>
2026-06-17 14:16:17 +0900

    部屋状態ページのテスト追加

---
+++ src/__test__/Room_status.spec.tsx
@@ -0,0 +1,73 @@
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter} from "react-router-dom"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import userEvent from "@testing-library/user-event";
+import { Rooms_status } from "../components/organisms/Rooms_status";
+
+
+const mockLogout =vi.fn();
+const mockNavigate = vi.fn();
+
+vi.mock("../context/AuthContext",()=>({
+    useAuth:()=>({
+        user: {name: "test", login_id: "test" , role :"admin"},
+        logout: mockLogout,
+    })
+}))
+
+vi.mock("react-router-dom",async()=>{
+    const actual = await vi.importActual("react-router-dom");
+    return{
+        ...actual,
+        useNavigate: () => mockNavigate,
+    }
+})
+
+
+vi.mock("../supabase.Client", () => ({
+    supabase: {
+      from: () => ({
+        select: () => ({
+          order:()=>({
+            data: [
+              { id: 1, room_number: "101", is_active: true, hide: false },
+              { id: 2, room_number: "102", is_active: false, hide: false },
+            ],
+            error:null,
+          })
+        }),
+      }),
+    },
+  }));
+
+describe("RoomsStatusページ", ()=>{
+    beforeEach(()=>{
+        mockNavigate.mockClear();
+        render(
+            <ChakraProvider value={defaultSystem}>
+        <MemoryRouter>
+          <Rooms_status />
+        </MemoryRouter>
+      </ChakraProvider>
+        )
+    })
+
+    test("RoomsStatusタイトルがある",()=>{
+        expect(screen.getByTestId("title")).toHaveTextContent("Rooms Status")
+    })
+
+    test("部屋番号と色が認識できる", async()=>{
+      const rooms = await screen.findAllByTestId(/room_number/)
+        expect(rooms).toHaveLength(2);
+        expect(screen.getByTestId("room_number_active")).toHaveTextContent("101")
+        expect(screen.getByTestId("room_number_inactive")).toHaveTextContent("102")
+    })
+
+    test("戻るボタンが機能する",async()=>{
+        const user = userEvent.setup();
+        await user.click(screen.getByText("Back"))
+        expect(mockNavigate).toHaveBeenCalledWith("/home")
+    })
+
+    
+})
